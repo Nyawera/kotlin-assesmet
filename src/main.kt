d746c908ed96882de707ac913c4c78d803c44aaf
Nyawera-Tut <nyaweratutchuol@gmail.com>
2022-03-10 09:03:13 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,31 @@
+fun main(){
+
+
+}
+////fun add() {
+////    var name = ("codeHive")
+////    var code1= "school"[5]
+////    var code2= "school"[6]
+////    var code3= "school"[7]
+////    var name=("code1","code2","code3")
+////
+//}
+        fun modulus(Int:5,Int:3,Int:)
+        var modulus(5/3)
+         return
+
+fun names(a:String,b:String)String:{
+    var=
+
+}
+
+
+
+
+
+
+
+
+
+
+
